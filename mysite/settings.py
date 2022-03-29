6739b74bd7c5bc4e567b29037a3e104172df56a5
Ray Corkran <raycorkran@gmail.com>
2022-03-29 13:39:22 -0400

    Added view and template for detailed blog post as well as CSS for the site,

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join('static')),
 # STATIC_ROOT BASE_DIR / 'static'
 
 # Default primary key field type
